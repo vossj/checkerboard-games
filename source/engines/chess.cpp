7530fc656fb54039be9670e97b52eb068385946a
Jimmy Voss <vossj@cse.ohio-state.com>
2016-05-29 01:21:58 -0400

    Setting up file for creating chess positions / games

---
+++ source/engines/chess.cpp
@@ -0,0 +1,111 @@
+#include "chess.h"
+
+#include <cassert>
+#include <iostream>
+
+using namespace Chess;
+using namespace std;
+
+Move::Move()
+{
+    ShortCastle = false;
+    LongCastle = false;
+    EnPassant = false;
+    StartSquare = "";
+    EndSquare = "";
+    CapturedPiece = Pieces::NONE;
+    Promotion = Pieces::NONE;
+}
+
+Position::Position()
+{
+    // Set up an empty board
+    for (int i = 0 ; i < 8 ; ++i )
+	for (int j = 0 ; j < 8 ; ++j )
+	    Board[i][j] = Pieces::NONE;
+}
+
+bool Position::ValidCoordinate( const string & coord )
+{
+    // Sanity check length first
+    if (coord.length() != 2)
+	return false;
+
+    // Check actual coordinate is valid
+    return ('a' <= coord[0]) && (coord[0] <= 'h') &&
+	('1' <= coord[1]) && (coord[1] <= '8');
+}
+
+Piece & Position::operator[]( const string & coord )
+{
+    assert( Position::ValidCoordinate(coord) );
+
+    return Board[coord[0]-'a'][coord[1]-'1'];
+}
+
+const Piece Position::operator[]( const string & coord ) const
+{
+    assert( Position::ValidCoordinate( coord ) );
+
+    return Board[coord[0]-'a'][coord[1]-'1'];
+}
+
+void Position::Print( ostream & out )
+{
+    for ( char row = '8' ; row >= '1' ; row-- ) {
+	// print row labels
+	out << row;
+
+	for ( char col = 'a' ; col <= 'h' ; col++ ) {
+	    string coord = "";
+	    coord.push_back(col);
+	    coord.push_back(row);
+	    
+	    Piece p = (*this)[coord];
+	    
+	    out << " ";
+
+	    // Actually print the piece
+	    if (p == Pieces::NONE) {
+		out << "  ";
+		continue;
+	    }
+	    
+	    if (p & PieceFlags::BLACK)
+		out << "B";
+	    else
+		out << "W";
+
+	    if (p & PieceFlags::PAWN)
+		out << "P";
+	    else if (p & PieceFlags::KNIGHT)
+		out << "N";
+	    else if (p & PieceFlags::BISHOP)
+		out << "B";
+	    else if (p & PieceFlags::ROOK)
+		out << "R";
+	    else if (p & PieceFlags::QUEEN)
+		out << "Q";
+	    else if (p & PieceFlags::KING)
+		out << "K";
+	    else
+		assert(false);
+	}
+	out << endl;
+    }
+
+    // Print column labels
+    out << " ";
+    for ( char col = 'a' ; col <= 'h' ; col++ ) {
+	out << " " << col << " ";
+    }
+    out << endl;
+}
+
+int main()
+{
+    Position P;
+    P.Print( cout );
+    
+    return 0;
+}
---
+++ source/engines/chess.h
@@ -0,0 +1,78 @@
+#ifndef __CHESS_H__
+#define __CHESS_H__
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+namespace Chess
+{
+    /* Enumeration of all possible chess piece types.
+       
+       Pieces properties are represented as binary flags within a
+       integer type byte in order to minimize storage / maximize
+       copying speed.
+    */
+    typedef unsigned char Piece;
+
+    namespace PieceFlags {
+	// Binary flags used to construct individual pieces.
+	const Piece NONE = 0;
+	const Piece PAWN = 1;
+	const Piece KNIGHT = 1 << 1;
+	const Piece BISHOP = 1 << 2;
+	const Piece ROOK = 1 << 3;
+	const Piece QUEEN = 1 << 4;
+	const Piece KING = 1 << 5;
+	const Piece BLACK = 1 << 6; // white when this flag is off.
+    };
+
+    namespace Pieces {
+	// Declarations of actual pieces
+	const Piece NONE = 0;
+	const Piece WHITE_PAWN = PieceFlags::PAWN;
+	const Piece BLACK_PAWN = PieceFlags::PAWN | PieceFlags::BLACK;
+	const Piece WHITE_KNIGHT = PieceFlags::KNIGHT;
+	const Piece BLACK_KNIGHT = PieceFlags::KNIGHT | PieceFlags::BLACK;
+	const Piece WHITE_BISHOP = PieceFlags::BISHOP;
+	const Piece BLACK_BISHOP = PieceFlags::BISHOP | PieceFlags::BLACK;
+	const Piece WHITE_ROOK = PieceFlags::ROOK;
+	const Piece BLACK_ROOK = PieceFlags::ROOK | PieceFlags::BLACK;
+	const Piece WHITE_QUEEN = PieceFlags::QUEEN;
+	const Piece BLACK_QUEEN = PieceFlags::QUEEN | PieceFlags::BLACK;
+	const Piece WHITE_KING = PieceFlags::KING;
+	const Piece BLACK_KING = PieceFlags::KING | PieceFlags::BLACK;
+    };
+
+    class Move
+    {
+    public:
+	Move();
+	bool ShortCastle, LongCastle, EnPassant;
+	std::string StartSquare, EndSquare;
+	Piece CapturedPiece, Promotion;
+    };
+
+    class Position
+    {
+    public:
+	Position();
+	void Print( std::ostream & out );
+	
+	Piece & operator[]( const std::string & coord );
+	const Piece operator[]( const std::string & coord ) const;
+	    
+	static bool ValidCoordinate( const std::string & coord );
+	/* Returns whether a coordinate is a valid location on a chess
+	 * board.  Coordinates should be compatible with algebraic
+	 * chess notation.
+	 */
+	
+    protected:	
+	Piece Board[8][8];
+	
+    };
+
+}
+
+#endif
